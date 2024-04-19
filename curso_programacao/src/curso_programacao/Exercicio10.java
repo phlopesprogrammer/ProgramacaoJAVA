7b82902ed3c3088a4a95b6090c112cf1107deed8
Paulinho <Paulinho@DESKTOP-6K9KFJ9>
2024-04-19 12:01:52 -0300

    Calculo do Imposto de Renda

--- curso_programacao/src/curso_programacao/Exercicio10.java
+++ curso_programacao/src/curso_programacao/Exercicio10.java
@@ -4,30 +4,31 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio10 {
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
+
 		Scanner sc = new Scanner(System.in);
 
-		double salario = sc.nextDouble();
 		double imposto;
-		if (salario == 3002.00) {
-			imposto = 1000.00 * 0.08 + 2.00 * 0.18;
-			System.out.println();
-		} else if (salario >= 0.0 && salario <= 2000.00) {
-			System.out.println("Isento");
-		} else if (salario > 2000.00 && salario <= 3000.00) {
-			imposto = salario * (8 / 100);
-			System.out.println(imposto);
-		} else if (salario > 3000.00 && salario <= 4500.00) {
-			imposto = salario * (18 / 100);
-			System.out.println(imposto);
+
+		double salario = sc.nextDouble();
+
+		if (salario > 4500.00) {
+			imposto = (1000 * 0.08) + (1500 * 0.18) + (salario - 4500) * 0.28;
+			System.out.printf(" Imposto : %.2f%n ", imposto);
+		} else if (salario > 3000) {
+			imposto = (1000 * 0.08) + (salario - 3000.00) * 0.18 ;
+			System.out.printf(" Imposto : %.2f%n ", imposto);
+		} else if (salario > 2000) {
+			imposto = 1000 * 0.08;
+			System.out.printf(" Imposto : %.2f%n ", imposto);
 		} else {
-			imposto = salario * (28 / 100);
-			System.out.println(imposto);
+			System.out.println("Isento");
 		}
+		
+
 		sc.close();
 	}
 
