5a6c70d1912ff7eb8daaed9d8a4eaf2801ef5a82
Paulinho <Paulinho@DESKTOP-6K9KFJ9>
2024-04-22 13:00:25 -0300

    swiftCase

---
+++ curso_programacao/src/curso_programacao/Exercicio13swiftCase.java
@@ -0,0 +1,48 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Exercicio13swiftCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		String dia;
+
+		switch (x) {
+		case 1:
+			dia = "Domingo";
+			break;
+		case 2:
+			dia = "Segunda";
+			break;
+		case 3:
+			dia = "Terça";
+			break;
+		case 4:
+			dia = "Quarta";
+			break;
+		case 5:
+			dia = "Quinta";
+			break;
+		case 6:
+			dia = "Sexta";
+			break;
+		case 7:
+			dia = "Sábado";
+			break;
+		default:
+			dia = "Inválido";
+			break;
+		}
+
+		System.out.println(dia);
+		sc.close();
+
+	}
+
+}
