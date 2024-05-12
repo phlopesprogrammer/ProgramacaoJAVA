ed4e977b959d8d3984199c89461ffb7df69300a6
phlopesprogrammer <phlopesprogrammer@gmail.com>
2024-05-12 11:31:58 -0300

    for

---
+++ curso_programacao/src/curso_programacao/Exercicio31for.java
@@ -0,0 +1,24 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Exercicio31for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		int fat = 1;
+		for (int i=1; i<=n; i++) {
+			fat = fat * i;
+		}
+		
+		System.out.println(fat);
+		
+		sc.close();
+	}
+}
