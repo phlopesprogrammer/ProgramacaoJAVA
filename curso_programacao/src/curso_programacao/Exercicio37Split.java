a9e375a42feed9419306a1de05b56f6738ce49b9
phlopesprogrammer <phlopesprogrammer@gmail.com>
2024-05-13 17:46:33 -0300

    Split

---
+++ curso_programacao/src/curso_programacao/Exercicio37Split.java
@@ -0,0 +1,16 @@
+package curso_programacao;
+
+public class Exercicio37Split {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "potato apple lemon" ;
+		String [] vector = s.split(" ");
+		
+		System.out.println(vector[0]);
+		System.out.println(vector[1]);
+		System.out.println(vector[2]);
+	}
+
+}
